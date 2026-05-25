922bd548792226ad6374ef8d9fe178105bdc7e4a
mcgonzalc <>
2026-05-25 02:14:35 +0200

    feat: added way to make keys persistent

--- src/generateKeys.ts
+++ src/generateKeys.ts
@@ -1,27 +1,48 @@
 import { RsaPublicKey, RsaPrivateKey, generateKeyPair } from "rsa";
+import fs from "fs";
+import path from "path";
 
-const keypair = generateKeyPair(2048);
-export const publicKey = keypair.publicKey;
-export const privateKey = keypair.privateKey;
+/**
+ * Persistimos el par de claves para que NO cambie en cada arranque.
+ * Si cambiara, el Agregador (que cachea la pública al levantarse) cifraría
+ * con una pública que ya no corresponde a esta privada, y el descifrado
+ * devolvería un bigint erróneo.
+ */
+const KEY_PATH = path.join(process.cwd(), "keys.json");
+
+let publicKey: RsaPublicKey;
+let privateKey: RsaPrivateKey;
+
+if (fs.existsSync(KEY_PATH)) {
+  const saved = JSON.parse(fs.readFileSync(KEY_PATH, "utf8")) as {
+    n: string;
+    e: string;
+    d: string;
+  };
+  publicKey = new RsaPublicKey(BigInt(saved.n), BigInt(saved.e));
+  privateKey = new RsaPrivateKey(BigInt(saved.n), BigInt(saved.d));
+} else {
+  const keypair = generateKeyPair(2048);
+  publicKey = keypair.publicKey;
+  privateKey = keypair.privateKey;
+  fs.writeFileSync(
+    KEY_PATH,
+    JSON.stringify({
+      n: publicKey.n.toString(),
+      e: publicKey.e.toString(),
+      d: privateKey.d.toString(),
+    })
+  );
+}
+
+export { publicKey, privateKey };
 
 export const publicKeyJson = {
-    n: publicKey.n.toString(),
-    e: publicKey.e.toString()
+  n: publicKey.n.toString(),
+  e: publicKey.e.toString(),
 };
 
 export const privateKeyJson = {
-    n: privateKey.n.toString(),
-    d: privateKey.d.toString()
+  n: privateKey.n.toString(),
+  d: privateKey.d.toString(),
 };
-
-// const publicKeyString = JSON.stringify(publicKeyJson);
-// const privateKeyString = JSON.stringify(privateKeyJson);
-
-// const message = 15478412n;
-// const encryptedMsg = publicKey.encrypt(message);
-// const decryptedMsg = privateKey.decrypt(encryptedMsg);
-
-// console.log("Original: ", message);
-// console.log("Cifrado:  ", encryptedMsg);
-// console.log("Descifrado:", decryptedMsg);
-// console.log("Correcto: ", message === decryptedMsg);
--- src/serverEnergetica.ts
+++ src/serverEnergetica.ts
@@ -12,6 +12,14 @@ const port = 4000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  if (req.method === "OPTIONS") { res.sendStatus(204); return; }
+  next();
+});
+
 // Rutas Rest
 app.use("/", energeticaRoutes);
 
